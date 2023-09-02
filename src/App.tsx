6e6786a544e3ce1791212331df5e1452fd6876ee
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-01 23:09:10 -0400

    Added step state and made more changes to base styling
    I added step state and functions for incrementing and decrementing its value. I also
    added a placeholder array that will eventually hold the built out form sections. I
    styled the body and the core container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import StepView from "./components/StepView/StepView";
+
 function App() {
-  return <div></div>;
+  const [stepIndex, setStepIndex] = useState(0);
+
+  const stepComponents = [<div>Personal Info</div>, <div>Plans</div>, <div>Add Ons</div>, <div>Summary</div>];
+
+  function incrementStepIndex() {
+    setStepIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+
+      if (newIndex < stepComponents.length) {
+        return newIndex;
+      }
+
+      return 0;
+    });
+  }
+
+  function decrementStepIndex() {
+    setStepIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+
+      if (newIndex >= 0) {
+        return newIndex;
+      }
+
+      return stepComponents.length - 1;
+    });
+  }
+
+  return (
+    <main className="container">
+      <StepView currentStep={stepIndex} />
+    </main>
+  );
 }
 
 export default App;
