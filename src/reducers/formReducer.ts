1399667fa44a81e37c8c6a1af8a0b6770dd2ae7d
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-02 20:50:56 -0400

    Added invalidate actions
    Added invalidate action type and cases to the reducer switch statement.

--- src/reducers/formReducer.ts
+++ src/reducers/formReducer.ts
@@ -1,20 +1,30 @@
 import FormState from "../types/FomState";
 import FormAction from "../types/FormAction";
 
-function formReducer(state: FormState, action: FormAction) {
-  const { type, payload } = action;
+function formReducer(state: FormState, action: FormAction): FormState {
+  const { type } = action;
   const newState: FormState = { ...state };
 
   switch (type) {
     case "SET_EMAIL":
-      newState.email = { ...state.email, currentValue: payload };
+      newState.email = { ...action.payload };
       return newState;
     case "SET_NAME":
-      newState.name = { ...state.name, currentValue: payload };
+      newState.name = { ...action.payload };
       return newState;
     case "SET_PHONE_NUMBER":
-      newState.phoneNumber = { ...state.phoneNumber, currentValue: payload };
+      newState.phoneNumber = { ...action.payload };
+      return newState;
+    case "SET_EMAIL_INVALID":
+      newState.email = { ...newState.email, isValid: false };
+      return newState;
+    case "SET_NAME_INVALID":
+      newState.name = { ...newState.name, isValid: false };
+      return newState;
+    case "SET_PHONE_NUMBER_INVALID":
+      newState.phoneNumber = { ...newState.phoneNumber, isValid: false };
       return newState;
   }
 }
+
 export default formReducer;
--- src/types/FormAction.ts
+++ src/types/FormAction.ts
@@ -1,6 +1,14 @@
-interface FormAction {
-  type: "SET_EMAIL" | "SET_NAME" | "SET_PHONE_NUMBER";
-  payload: string;
+import FormField from "./FormField";
+
+interface InvalidateAction {
+  type: "SET_EMAIL_INVALID" | "SET_NAME_INVALID" | "SET_PHONE_NUMBER_INVALID";
 }
+
+interface SetAction {
+  type: "SET_EMAIL" | "SET_NAME" | "SET_PHONE_NUMBER";
+  payload: FormField;
+}
+
+type FormAction = SetAction | InvalidateAction;
 
 export default FormAction;
