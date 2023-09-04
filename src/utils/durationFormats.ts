9b8207d0ea8184910fcf288a9ede91b6c1680e72
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-04 13:12:38 -0400

    Added object to hold the different versions of duration strings that will be used in the UI

---
+++ src/utils/durationFormats.ts
@@ -0,0 +1,14 @@
+const durationFormats = {
+  monthly: {
+    short: "mo",
+    capitalized: "Monthly",
+    per: "month",
+  },
+  yearly: {
+    short: "yr",
+    capitalized: "Yearly",
+    per: "year",
+  },
+};
+
+export default durationFormats;
